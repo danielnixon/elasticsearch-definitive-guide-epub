8bce4ba7b91bcf8d53760e3c119c9497423e4452
Daniel Nixon <dan.nixon@gmail.com>
2015-08-21 12:33:04 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,3 +11,9 @@ command -v a2x >/dev/null 2>&1 || { echo "Missing command 'a2x'. Try 'brew insta
 export XML_CATALOG_FILES=/usr/local/etc/xml/catalog
 
 a2x -d book -D . -f epub ./elasticsearch-definitive-guide/book.asciidoc -v
+
+if hash ebook-convert 2>/dev/null; then
+    ebook-convert book.epub book.mobi
+else
+    echo "Missing command 'ebook-convert', cannot convert to mobi. Try 'brew cask install calibre'."
+fi
